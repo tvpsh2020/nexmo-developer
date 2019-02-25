997946971711f6c9d19ccf77c96df6ff673a63e9
benhayehudi <bengreenberg@gmail.com>
2019-02-25 15:47:40 +0200

    added featured_session partial and new featured_video helper method

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -12,4 +12,14 @@ module VideoHelper
 
     video_url
   end
+
+  def featured_video
+    videos = Session.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month)
+    
+    if videos.length == 0
+      videos = Session.where("created_at >= :month", {:month => 1.month.ago})
+    end
+    sorted = videos.order(:created_at)
+    return sorted
+  end
 end
